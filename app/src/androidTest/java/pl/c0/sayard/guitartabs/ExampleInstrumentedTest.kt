69f0d264b019ee98c83fb18451d32c7ff6f4bc70
Karol Gracz <graczu42@gmail.com>
2017-05-22 19:37:17 +0200

    init

---
+++ app/src/androidTest/java/pl/c0/sayard/guitartabs/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package pl.c0.sayard.guitartabs
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("pl.c0.sayard.guitartabs", appContext.packageName)
+    }
+}
