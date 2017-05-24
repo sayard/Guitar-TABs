b68cb8b38b58e23eabda1e3432b27d1c35e9f92a
Karol Gracz <graczu42@gmail.com>
2017-05-24 23:12:30 +0200

    tabs can now be searched

--- app/src/main/java/pl/c0/sayard/guitartabs/MainActivity.kt
+++ app/src/main/java/pl/c0/sayard/guitartabs/MainActivity.kt
@@ -2,12 +2,21 @@ package pl.c0.sayard.guitartabs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        TabsCrawlerTask(this, this).execute("https://www.ultimate-guitar.com/search.php?search_type=title&order=&value=revenga")
+        val searchBar: EditText = findViewById(R.id.search_bar) as EditText
+        val searchButton: Button = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            val searchQuery = searchBar.text.replace("\\s".toRegex(), "+")
+            TabsCrawlerTask(this, this).execute("https://www.ultimate-guitar.com/search.php?search_type=title&order=&value=$searchQuery")
+        }
     }
 }
