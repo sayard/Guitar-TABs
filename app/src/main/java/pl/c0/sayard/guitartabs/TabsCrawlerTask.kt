82868bf540c5e05c735c621a17399d6381653814
Karol Gracz <graczu42@gmail.com>
2017-05-25 10:54:30 +0200

    added error displaying when no tabs found

--- app/src/main/java/pl/c0/sayard/guitartabs/TabsCrawlerTask.kt
+++ app/src/main/java/pl/c0/sayard/guitartabs/TabsCrawlerTask.kt
@@ -5,9 +5,9 @@ import android.content.Context
 import android.os.AsyncTask
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import org.jsoup.HttpStatusException
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -19,10 +19,11 @@ import org.jsoup.select.Elements
 class TabsCrawlerTask constructor(val activity: Activity, val context: Context): AsyncTask<String, Void, Elements>() {
 
     var progressBar: ProgressBar = activity.findViewById(R.id.progress_bar) as ProgressBar
-
+    var errorMessage: TextView = activity.findViewById(R.id.error_message) as TextView
 
     override fun onPreExecute() {
         progressBar.visibility = View.VISIBLE
+        errorMessage.visibility = View.GONE
     }
 
     override fun doInBackground(vararg params: String?): Elements? {
@@ -37,15 +38,15 @@ class TabsCrawlerTask constructor(val activity: Activity, val context: Context):
     }
 
     override fun onPostExecute(results: Elements?) {
+        progressBar.visibility = View.GONE
         if(results != null){
-            progressBar.visibility = View.GONE
             val recyclerView: RecyclerView = activity.findViewById(R.id.tabs_list) as RecyclerView
             val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
             recyclerView.layoutManager = layoutManager
             val adapter: TabsAdapter = TabsAdapter(context, results)
             recyclerView.adapter = adapter
         }else{
-            //TODO display error message
+            errorMessage.visibility = View.VISIBLE
         }
     }
 }
