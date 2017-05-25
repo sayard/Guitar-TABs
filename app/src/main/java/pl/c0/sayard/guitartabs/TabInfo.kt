d6d95b1ba2f80a435fa3d6f2db90ea832613142f
Karol Gracz <graczu42@gmail.com>
2017-05-25 10:31:10 +0200

    changer tabinfo into data class

--- app/src/main/java/pl/c0/sayard/guitartabs/TabInfo.kt
+++ app/src/main/java/pl/c0/sayard/guitartabs/TabInfo.kt
@@ -3,8 +3,5 @@ package pl.c0.sayard.guitartabs
 /**
  * Created by Karol on 24.05.2017.
  */
-class TabInfo constructor(link: String, name: String, type: String){
-    var link: String = link
-    var name: String = name
-    var type: String = type
+data class TabInfo constructor(val link: String, val name: String, val type: String){
 }
